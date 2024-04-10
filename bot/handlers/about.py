71ccfd4c41e60c2fadb877f047269e0645691437
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-10 18:49:40 +0500

    add cquery handlers

--- bot/handlers/about.py
+++ bot/handlers/about.py
@@ -1,25 +1,24 @@
 from aiogram.filters import Command
-from aiogram import types, Router
+from aiogram import types, Router, F
 
-from bot.keyboards.main_menu import MainMenu
+from bot.keyboards.inline_menu import InlineMenu
 
 about_router = Router()
 
 msg = 'Здесь будет описание бота!'
+items = {
+    'Главная': 'start',
+    'Начать диалог': 'ask',
+}
 
 
 @about_router.message(Command('about'))
 async def about(message: types.Message) -> None:
-    menu = MainMenu().place()
-    await message.answer(
-        msg,
-        reply_markup=menu,
-    )
+    menu = InlineMenu()
+    await message.answer(msg, reply_markup=menu.place(**items))
 
 
+@about_router.callback_query(F.data == 'about')
 async def about_callback(callback: types.CallbackQuery) -> None:
-    menu = MainMenu().place()
-    await callback.message.answer(
-        msg,
-        reply_markup=menu,
-    )
+    menu = InlineMenu()
+    await callback.message.answer(msg, reply_markup=menu.place(**items))
--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,27 +1,34 @@
 from aiogram.filters import Command
-from aiogram import types, Router
+from aiogram import types, Router, F
 
-from bot.keyboards.main_menu import MainMenu
+from bot.keyboards.inline_menu import InlineMenu
+
 
 start_router = Router()
+
+items = {
+    'О боте': 'about',
+    'Начать диалог': 'ask',
+}
 
 
 @start_router.message(Command('start'))
 async def start(message: types.Message):
-    menu = MainMenu().place()
+    menu = InlineMenu()
     await message.answer(
         f'Привет, {message.from_user.first_name},\n\n'
         f'Меня зовут Aio,\n'
         f'Я могу быть полезен как AI ассистент!\n\n'
         f'🔽 Выбери опцию использования 🔽',
-        reply_markup=menu,
+        reply_markup=menu.place(**items),
     )
 
 
+@start_router.callback_query(F.data == 'start')
 async def start_callback(callback: types.CallbackQuery) -> None:
-    menu = MainMenu().place()
+    menu = InlineMenu()
     await callback.message.answer(
         'Добро пожаловать на главную страницу!\n\n'
         '🔽 Выбери опцию использования 🔽',
-        reply_markup=menu,
+        reply_markup=menu.place(**items),
     )
