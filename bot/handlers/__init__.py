dfedf3a9ba98030a7e6cd7d7fe7189cc9d59aad2
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-10 18:46:50 +0500

    remove cquery register

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,6 +1,6 @@
-from aiogram import Dispatcher, F
-from .start import start_router, start_callback
-from .about import about_router, about_callback
+from aiogram import Dispatcher
+from .start import start_router
+from .about import about_router
 from .gpt import gpt_router
 
 
@@ -8,8 +8,3 @@ def register_routes(dp: Dispatcher) -> None:
     dp.include_router(start_router)
     dp.include_router(about_router)
     dp.include_router(gpt_router)
-
-
-def register_callback_query(dp: Dispatcher) -> None:
-    dp.callback_query.register(about_callback, F.data == 'about')
-    dp.callback_query.register(start_callback, F.data == 'start')
