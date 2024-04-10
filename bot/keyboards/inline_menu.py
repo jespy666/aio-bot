53179c2a802e54b35adf7ee7c64d9551bab6e186
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-10 18:45:45 +0500

    ignore pycache related files

--- bot/keyboards/inline_menu.py
+++ bot/keyboards/inline_menu.py
@@ -5,11 +5,7 @@ class MainMenu:
 
     def __init__(self):
         self.kb = InlineKeyboardBuilder()
-        self.items = {
-            'О боте': 'about',
-            'Главная': 'start',
-            'Задать вопрос': 'ask',
-        }
+        self.items = {}
 
     def place(self):
         for item, callback in self.items.items():
