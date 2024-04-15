5d419976ee0382ec658b6b18855abdac06164d47
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-15 20:22:21 +0500

    add template for dalle-3

---
+++ ai/image.py
@@ -0,0 +1,16 @@
+# from openai import OpenAI
+# from config import config
+#
+#
+# client = OpenAI(api_key=config.OPENAI_KEY)
+#
+# response = client.images.generate(
+#   model="dall-e-3",
+#   prompt="autistic wife",
+#   size="1024x1024",
+#   quality="standard",
+#   n=1,
+# )
+#
+# image_url = response.data[0].url
+# print(image_url)
