8dccef204189d210465f62cb1f69171892fdfe90
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-17 19:13:05 +0500

    refactor crud

--- storage/crud.py
+++ storage/crud.py
@@ -1,42 +1,40 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
 
-from storage.models import User
+from storage.models import User, Balance
 
 
-class UserCRUD:
+async def create_user(session: AsyncSession, name: str, pk: int) -> None:
+    if not name:
+        name = str(pk)
+    user = User(id=pk, name=name)
+    balance = Balance(user_id=pk)
+    session.add_all((user, balance))
+    await session.commit()
 
-    def __init__(self, db_url: str):
-        self.engine = create_async_engine(db_url, echo=True)
-        self.async_session = async_sessionmaker(bind=self.engine,
-                                                expire_on_commit=False)
 
-    async def create_user(self, name: str, pk: int) -> None:
-        if not name:
-            name = str(pk)
-        async with self.async_session() as session:
-            async with session.begin():
-                stmt = select(User).filter(User.id == pk)
-                response = await session.execute(stmt)
-                if response.fetchone():
-                    return
-            user = User(id=pk, name=name)
-            session.add(user)
-            await session.commit()
+async def get_user(session: AsyncSession, pk: int) -> User | None:
+    stmt = select(User).where(User.id == pk)
+    user: User | None = await session.scalar(stmt)
+    return user
 
-    async def get_user(self, pk: int) -> User:
-        async with self.async_session() as session:
-            async with session.begin():
-                stmt = select(User).filter(User.id == pk)
-                response = await session.execute(stmt)
-                user = response.first()
-                return user[0]
 
-    async def set_balance(self, pk: int, net_balance: float) -> None:
-        async with self.async_session() as session:
-            async with session.begin():
-                stmt = update(User).where(User.id == pk).values(
-                    balance=net_balance
-                )
-                await session.execute(stmt)
-                await session.commit()
+async def get_balance(session: AsyncSession, pk: int) -> Balance:
+    stmt = select(Balance).where(Balance.user_id == pk)
+    balance: Balance = await session.scalar(stmt)
+    return balance
+
+
+# async def get_user(session: AsyncSession, pk: int) -> User:
+#     async with session.begin():
+#         stmt = select(User).filter(User.id == pk)
+#         response = await session.execute(stmt)
+#         user = response.first()
+#         return user[0]
+#
+#
+# async def set_balance(session: AsyncSession, pk: int, net: float) -> None:
+#     async with session.begin():
+#         stmt = update(User).where(User.id == pk).values(balance=net)
+#         await session.execute(stmt)
+#         await session.commit()
