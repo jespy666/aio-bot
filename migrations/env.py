86c7458366ca18de431a4c9e2f54b22215515510
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-17 19:12:30 +0500

    fix alembic config

--- migrations/env.py
+++ migrations/env.py
@@ -18,9 +18,9 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from storage import models
+from storage.models.base import Base
 
-target_metadata = models.Base.metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
