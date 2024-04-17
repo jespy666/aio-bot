72134bbf172c29467119210d15b1c75fb0f84124
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-17 19:13:32 +0500

    add user check decorator

--- bot/wrappers.py
+++ bot/wrappers.py
@@ -2,23 +2,44 @@ from functools import wraps
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
-from crud import UserCRUD
-from config import config
+from storage.models import User
+from storage import crud
+from storage.session import Session
 
 
-def balance_control(func):
-
+def check_balance(func):
     @wraps(func)
-    async def wrapper(message: Message, state: FSMContext):
-        session = UserCRUD(config.DATABASE_URL)
-        await session.create_user(message.chat.first_name, message.chat.id)
-        user = await session.get_user(message.chat.id)
-        if user.balance <= 0.000001:
+    async def wrapper(message: Message, state: FSMContext, **kwargs):
+        context = await state.get_data()
+        session = kwargs.get('session')
+        if not session:
+            session = Session().get_session()
+        # for performance optimization, check the user from FSM context
+        user: User = context.get('user')
+        if not user:
+            user: User = await crud.get_user(session, message.chat.id)
+        balance: float = user.balance
+        await state.update_data(user=user, session=session)
+        if balance <= 0.000001:
             await message.answer(
                 'У вас недостаточно средств!\n'
                 'Пополните баланс...'
             )
             await state.clear()
             return
-        return await func(message, state)
+        return await func(message, state, **kwargs)
+    return wrapper
+
+
+def check_user(func):
+
+    @wraps(func)
+    async def wrapper(message: Message, *args, **kwargs):
+        session = await Session().get_session()
+        user_id = message.chat.id
+        user = await crud.get_user(session, user_id)
+        if not user:
+            await crud.create_user(session, user_id)
+        return await func(message, session=session, *args, **kwargs)
+
     return wrapper
