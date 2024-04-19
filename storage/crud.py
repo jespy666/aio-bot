bf31d7d1998d7a32d2d35d84f22918d74098b5c4
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-19 19:15:40 +0500

    crud only for users table

--- storage/crud.py
+++ storage/crud.py
@@ -1,15 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
 
-from storage.models import User, Balance
+from typing import Any
+
+from storage.models import User
 
 
 async def create_user(session: AsyncSession, name: str, pk: int) -> None:
     if not name:
         name = str(pk)
     user = User(id=pk, name=name)
-    balance = Balance(user_id=pk)
-    session.add_all((user, balance))
+    session.add(user)
     await session.commit()
 
 
@@ -19,22 +20,7 @@ async def get_user(session: AsyncSession, pk: int) -> User | None:
     return user
 
 
-async def get_balance(session: AsyncSession, pk: int) -> Balance:
-    stmt = select(Balance).where(Balance.user_id == pk)
-    balance: Balance = await session.scalar(stmt)
-    return balance
-
-
-# async def get_user(session: AsyncSession, pk: int) -> User:
-#     async with session.begin():
-#         stmt = select(User).filter(User.id == pk)
-#         response = await session.execute(stmt)
-#         user = response.first()
-#         return user[0]
-#
-#
-# async def set_balance(session: AsyncSession, pk: int, net: float) -> None:
-#     async with session.begin():
-#         stmt = update(User).where(User.id == pk).values(balance=net)
-#         await session.execute(stmt)
-#         await session.commit()
+async def update_user(session: AsyncSession, user_id: int, **kwargs) -> None:
+    stmt = update(User).where(User.id == user_id).values(**kwargs)
+    await session.execute(stmt)
+    await session.commit()
