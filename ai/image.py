8c70779e10a1e817e33742fbb6cded35fe322fab
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-24 20:20:27 +0500

    add dall-e image generator

--- ai/image.py
+++ ai/image.py
@@ -1,34 +1,33 @@
-# from io import BytesIO
-#
-# from openai import OpenAI
-# import base64
-# from PIL import Image
-#
-# # from .dialogue import DialogueBase
-#
-#
-# from config import config
-#
-#
-# client = OpenAI(api_key=config.OPENAI_KEY)
-# #
-# #
-# response = client.images.generate(
-#       model="dall-e-2",
-#       prompt="Красивый логотип нейросети",
-#       size="256x256",
-#       quality="standard",
-#       response_format='b64_json',
-#       n=1,
-# )
-# #
-# # image_url = response.data[0].url
-# # print(image_url)
-#
-# image_data = base64.b64decode(response.data[0].url)
-# image = Image.open(BytesIO(image_data))
-# print(image)
-#
-#
-# # class ImageDialogue(DialogueBase):
-# #     pass
+from typing import Literal
+
+from openai import OpenAI
+
+from .dialogue import DialogueBase
+
+from storage.models import User
+
+
+class ImageDialogue(DialogueBase):
+
+    def __init__(self, client: OpenAI, user: User, model: str) -> None:
+        super().__init__(client, user, model)
+
+    async def generate_image(
+            self,
+            prompt: str,
+            size: Literal[
+                "256x256", "512x512", "1024x1024", "1792x1024", "1024x1792"
+            ],
+            quality: Literal["standard", "hd"] = 'standard'
+    ) -> str:
+
+        self.check_requests_amount()
+        response = self.client.images.generate(
+            model=self.model,
+            prompt=prompt,
+            size=size,
+            quality=quality,
+            n=1,
+        )
+        image_url = response.data[0].url
+        return image_url
