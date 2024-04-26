6fb53b2a7854aba67201931373d219679ec873ff
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-04-26 20:31:53 +0500

    except input files errors

--- bot/wrappers.py
+++ bot/wrappers.py
@@ -6,7 +6,7 @@ from aiogram.fsm.context import FSMContext
 from bot import exceptions as e
 from bot.keyboards import InlineMenu
 
-from .states import TextStates
+from .states import TextStates, ImgEditStates, ImgGenStates
 
 
 def validators(func):
@@ -43,4 +43,64 @@ def validators(func):
             )
             await message.answer(msg, reply_markup=menu)
             await state.clear()
+        except e.IncorrectImageSizeError:
+            kb = context.get('sizes_kb')
+            msg = (
+                '🔴🔴🔴\n\n'
+                '<strong>Open AI не сможет отправить фото такого размера'
+                '</strong>'
+            )
+            msg2 = (
+                '<em>Пожалуйста выберите размер генерируемой картинки\n'
+                'из доступных вариантов ниже</em>'
+            )
+            await message.answer(msg, reply_markup=cancel_btn)
+            await message.answer(msg2, reply_markup=kb)
+            current_state = await state.get_state()
+            if current_state == ImgEditStates.erased:
+                await state.set_state(ImgEditStates.erased)
+            elif current_state == ImgGenStates.size:
+                await state.set_state(ImgGenStates.size)
+        except e.IncorrectImageFormatError:
+            msg = (
+                '🔴🔴🔴\n\n'
+                '<strong>Некорректный формат изображения</strong>\n\n'
+                '<em>Изображение должно быть в формате <strong>.PNG</strong>\n'
+                'Не забудте снять галочку с опции "Compress the image"\n\n'
+                'Отправьте изображение еще раз</em>'
+            )
+            await message.answer(msg, reply_markup=cancel_btn)
+            current_state = await state.get_state()
+            if current_state == ImgEditStates.original:
+                await state.set_state(ImgEditStates.original)
+            elif current_state == ImgEditStates.erased:
+                await state.set_state(ImgEditStates.erased)
+        except e.AspectRatioMismatchError:
+            msg = (
+                '🔴🔴🔴\n\n'
+                '<strong>Изображение должно быть квадратным!</strong>\n\n'
+                '<em>Длинна и ширина вашей картинки должны быть равны,\n'
+                'для корректной обработки Open AI\n\n'
+                'Отредактируйте ваше изображение и отправьте мне его еще раз\n'
+                '</em>'
+            )
+            await message.answer(msg, reply_markup=cancel_btn)
+            current_state = await state.get_state()
+            if current_state == ImgEditStates.original:
+                await state.set_state(ImgEditStates.original)
+            elif current_state == ImgEditStates.erased:
+                await state.set_state(ImgEditStates.erased)
+        except e.UnknownQualityError:
+            kb = context.get('quality_kb')
+            msg = (
+                '🔴🔴🔴\n\n'
+                '<strong>Вы ввели несуществующее качество</strong>'
+            )
+            msg2 = (
+                '<em>Пожалуйста, выберите уровень качества генерируемой\n'
+                'картинки из вариантов ниже</em>'
+            )
+            await message.answer(msg, reply_markup=cancel_btn)
+            await message.answer(msg2, reply_markup=kb)
+            await state.set_state(ImgGenStates.size)
     return wrapper
